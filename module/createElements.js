d05a861f83404d69eef067b02d9081249487300a
Sid-Straw <j80304xz@gmail.com>
2021-10-13 23:52:29 +0800

    feat: createPaginationItem

--- module/createElements.js
+++ module/createElements.js
@@ -1,10 +1,4 @@
-export default function createCard({
-  ServiceType,
-  StationName,
-  BikesCapacity,
-  StationAddress,
-  key = 0,
-}) {
+export function createCard({ ServiceType, StationName, BikesCapacity, StationAddress, key = 0 }) {
   return /*html*/ `
       <article class="flex flex-col shadow m-4 max-w-xs">
           <img src="https://source.unsplash.com/collection/1346951/400x200?sig=${key}" alt="" />
@@ -17,3 +11,37 @@ export default function createCard({
       </article>
     `
 }
+
+export function createPaginationItem( page, isActive ) {
+  const { value, action } = page
+  if(!action) {
+    return /*html*/ `
+      <button class="h-10 w-10 font-semibold text-gray-800 text-sm flex items-center justify-center ml-3">
+        ${value}
+      </button>
+    `
+  }
+  if(isActive) {
+    return /*html*/ `
+      <button
+        class="h-10 w-10 bg-blue-800 hover:bg-blue-600 font-semibold text-white text-sm flex items-center justify-center"
+        data-action="${action}"
+        data-value="${value}"
+      >
+        ${value}
+      </button>
+    `
+  }
+
+  return /*html*/ `
+    <button
+      class="h-10 w-10 font-semibold text-gray-800 hover:bg-blue-600 hover:text-white text-sm flex items-center justify-center"
+      data-action="${action}"
+      data-value="${value}"
+    >
+      ${value}
+    </button>
+  `
+}
+
+export default { createCard, createPaginationItem }
