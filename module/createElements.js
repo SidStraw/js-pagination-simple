3119c23451f0946d383b42a7d95278b06f22708d
Sid-Straw <j80304xz@gmail.com>
2021-10-15 01:30:27 +0800

    feat: createElements > add renderFunction

--- module/createElements.js
+++ module/createElements.js
@@ -1,3 +1,15 @@
+export function renderFunction(listDom, renderContent, contentData = []) {
+  if (contentData.length) listDom.innerHTML = updateElement([...contentData])
+
+  function updateElement(newData) {
+    if (newData === contentData) return
+    contentData = newData
+    listDom.innerHTML = newData.map(renderContent).join('')
+  }
+
+  return updateElement
+}
+
 export function createCard({ ServiceType, StationName, BikesCapacity, StationAddress, key = 0 }) {
   return /*html*/ `
       <article class="flex flex-col shadow m-4 max-w-xs">
@@ -12,16 +24,15 @@ export function createCard({ ServiceType, StationName, BikesCapacity, StationAdd
     `
 }
 
-export function createPaginationItem( page, isActive ) {
-  const { value, action } = page
-  if(!action) {
+export function createPaginationItem({ value, action, isActive }) {
+  if (!action) {
     return /*html*/ `
       <button class="h-10 w-10 font-semibold text-gray-800 text-sm flex items-center justify-center ml-3">
         ${value}
       </button>
     `
   }
-  if(isActive) {
+  if (isActive) {
     return /*html*/ `
       <button
         class="h-10 w-10 bg-blue-800 hover:bg-blue-600 font-semibold text-white text-sm flex items-center justify-center"
@@ -44,4 +55,4 @@ export function createPaginationItem( page, isActive ) {
   `
 }
 
-export default { createCard, createPaginationItem }
+export default { renderFunction, createCard, createPaginationItem }
