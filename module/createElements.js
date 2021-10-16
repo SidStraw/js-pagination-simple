e6c68c9173059f6cb4fe85fcc87fb02d4f3c3dbf
kayac-chang <egg734631@gmail.com>
2021-10-16 17:45:05 +0800

    refactor: 💡 improve a11y and component

--- module/createElements.js
+++ module/createElements.js
@@ -13,10 +13,14 @@ export function renderFunction(listDom, renderContent, contentData = []) {
 export function createCard({ ServiceType, StationName, BikesCapacity, StationAddress, key = 0 }) {
   return /*html*/ `
       <article class="flex flex-col shadow m-4 max-w-xs">
-          <img src="https://source.unsplash.com/collection/1346951/400x200?sig=${key}" alt="" />
+          <img 
+            src="https://source.unsplash.com/collection/1346951/400x200?sig=${key}" 
+            alt="${key} placeholder" 
+          />
+
           <div class="bg-white flex flex-col justify-start p-6">
               <p class="text-blue-700 text-sm font-bold uppercase pb-4">${ServiceType}</p>
-              <p class="text-3xl font-bold hover:text-gray-700 pb-4">${StationName?.Zh_tw}</p>
+              <h2 class="text-3xl font-bold hover:text-gray-700 pb-4">${StationName?.Zh_tw}</h2>
               <p class="uppercase text-gray-800 hover:text-black">站點最大車數： ${BikesCapacity}</p>
               <p class="text-sm pb-3">${StationAddress?.Zh_tw}</p>
           </div>
@@ -24,35 +28,58 @@ export function createCard({ ServiceType, StationName, BikesCapacity, StationAdd
     `
 }
 
-export function createPaginationItem({ value, action, isActive }) {
-  if (!action) {
-    return /*html*/ `
-      <button class="h-10 w-10 font-semibold text-gray-800 text-sm flex items-center justify-center ml-3">
-        ${value}
-      </button>
-    `
-  }
-  if (isActive) {
-    return /*html*/ `
-      <button
-        class="h-10 w-10 bg-blue-800 hover:bg-blue-600 font-semibold text-white text-sm flex items-center justify-center"
-        data-action="${action}"
-        data-value="${value}"
-      >
-        ${value}
-      </button>
-    `
-  }
+function clsx(...args) {
+  return args.filter(Boolean).join(" ")
+}
 
+function propsToAttribute(props) {
+  return Object.entries(props)
+    .reduce(
+      (array, [key, value]) => array.concat(`${key}="${value}"`),
+      []
+    )
+    .join(" ")
+}
+
+function Item({ value, className, ...props }) {
   return /*html*/ `
-    <button
-      class="h-10 w-10 font-semibold text-gray-800 hover:bg-blue-600 hover:text-white text-sm flex items-center justify-center"
-      data-action="${action}"
-      data-value="${value}"
+    <a 
+      href="?page=${value}"
+      onclick="return false"
+      class="${clsx(
+        "h-10 w-10",
+        "font-semibold text-sm",
+        "flex items-center justify-center ml-3",
+        className
+      )}"
+      
+      ${propsToAttribute(props)}
     >
       ${value}
-    </button>
+    </a>
   `
+}
+
+export function createPaginationItem({ value, action, isActive }) {
+  if (!action) {
+    return Item({ value, className: "text-gray-800" })
+  }
+
+  if (isActive) {
+    return Item({
+      value,
+      className: "bg-blue-800 hover:bg-blue-600 text-white",
+      "data-action": action,
+      "data-value": value,
+    })
+  }
+
+  return Item({
+    value,
+    className: "text-gray-800 hover:bg-blue-600 hover:text-white",
+    "data-action": action,
+    "data-value": value,
+  })
 }
 
 export default { renderFunction, createCard, createPaginationItem }
