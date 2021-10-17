4c00650d30919f8b5949bcb58e3216c9b2fa16f8
SidStraw <j80304xz@gmail.com>
2021-10-17 23:56:22 +0800

    feat: createCard > Modify information

--- module/createElements.js
+++ module/createElements.js
@@ -10,16 +10,16 @@ export function renderFunction(listDom, renderContent, contentData = []) {
   return updateElement
 }
 
-export function createCard({ ServiceType, StationName, BikesCapacity, StationAddress, key = 0 }) {
+export function createCard({ ServiceType, StationName, BikesCapacity, StationAddress, StationUID }) {
   return /*html*/ `
       <article class="flex flex-col shadow m-4 max-w-xs">
           <img 
-            src="https://source.unsplash.com/collection/1346951/400x200?sig=${key}" 
-            alt="${key} placeholder" 
+            src="https://source.unsplash.com/collection/1346951/400x200?sig=${StationUID}" 
+            alt="${StationUID} placeholder" 
           />
 
           <div class="bg-white flex flex-col justify-start p-6">
-              <p class="text-blue-700 text-sm font-bold uppercase pb-4">${ServiceType}</p>
+              <p class="text-blue-700 text-sm font-bold uppercase pb-4">YouBike ${ServiceType}.0</p>
               <h2 class="text-3xl font-bold hover:text-gray-700 pb-4">${StationName?.Zh_tw}</h2>
               <p class="uppercase text-gray-800 hover:text-black">站點最大車數： ${BikesCapacity}</p>
               <p class="text-sm pb-3">${StationAddress?.Zh_tw}</p>
